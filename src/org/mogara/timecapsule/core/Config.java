7fce5619f499a624598016e1360c23aff7b400bd
Takashiro <takashiro@qq.com>
2015-12-27 03:03:29 +0800

    Remove SIFT

--- src/org/mogara/timecapsule/core/Config.java
+++
@@ -1,7 +0,0 @@
-package org.mogara.timecapsule.core;
-
-import android.os.Environment;
-
-public class Config {
-	public static String STORAGE_PATH = Environment.getExternalStorageDirectory().getPath() + "/TimeCapsule";
-}
