2e093f5ec93ce29a95bc51bfec063787c95e426e
Takashiro <takashiro@qq.com>
2015-12-26 18:15:05 +0800

    Add Capsule

--- src/org/mogara/timecapsule/MainActivity.java
+++ src/org/mogara/timecapsule/MainActivity.java
@@ -1,5 +1,7 @@
 package org.mogara.timecapsule;
 
+import org.mogara.timecapsule.model.Capsule;
+
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -9,5 +11,13 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Thread thread = new Thread(){
+			public void run(){
+				Capsule capsule = new Capsule("1");
+				capsule.getHotspotKDFeaturePoint();
+			}
+		};
+		thread.start();
 	}
 }
