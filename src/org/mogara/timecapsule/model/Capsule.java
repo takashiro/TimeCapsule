ff5464300cea63931d037b5e0c9aff7dbe649767
Takashiro <takashiro@qq.com>
2015-12-26 22:06:56 +0800

    Call system camera

--- src/org/mogara/timecapsule/model/Capsule.java
+++ src/org/mogara/timecapsule/model/Capsule.java
@@ -30,92 +30,104 @@ import com.alibaba.simpleimage.analyze.sift.render.RenderImage;
 import com.alibaba.simpleimage.analyze.sift.scale.KDFeaturePoint;
 
 public final class Capsule {
-	
-	public static final String STORAGE_PATH = Environment.getExternalStorageDirectory().getPath() + "/TimeCapsule";
-	public static final String ROOT_PATH = STORAGE_PATH + "/hotspot";
-	public static final String REMOTE_PROCESSOR_IP = "192.168.1.104";
-	public static final int REMOTE_PROCESSOR_PORT = 9527;
-	public static final int MIN_MATCHED_KEY_NUM = 100;
+
+	private static final String STORAGE_PATH = Environment.getExternalStorageDirectory().getPath() + "/TimeCapsule";
+	private static final String ROOT_PATH = STORAGE_PATH + "/capsule";
+	private static final String REMOTE_PROCESSOR_IP = "192.168.1.104";
+	private static final int REMOTE_PROCESSOR_PORT = 9527;
+	private static final int MIN_MATCHED_KEY_NUM = 100;
+	private static final int LOCAL_SIFT_IMAGE_PIXEL_LIMIT = 100;
 
 	private String id = null;
 
-	public Capsule(){
+	public Capsule() {
 		this.id = generateId();
 	}
-	
-	public Capsule(String id){
+
+	public Capsule(String id) {
 		this.id = id;
 	}
-	
-	public String getPath(){
+
+	public String getId() {
+		return id;
+	}
+
+	public boolean mkdirs() {
+		File dir = new File(getPath());
+		return dir.mkdirs();
+	}
+
+	public String getPath() {
 		return ROOT_PATH + "/" + id;
 	}
-	
-	public String getRawImage(){
+
+	public String getRawImage() {
 		return getPath() + "/hotspot.jpg";
 	}
-	
-	public String getTextPath(){
+
+	public String getTextPath() {
 		return getPath() + "/intro.txt";
 	}
-	
-	public String getGalleryPath(){
+
+	public String getGalleryPath() {
 		return getPath() + "/images";
 	}
-	
-	public File[] getGalleryFiles(){
+
+	public File[] getGalleryFiles() {
 		File gallery = new File(getGalleryPath());
-		if(gallery.exists() && gallery.isDirectory()){
+		if (gallery.exists() && gallery.isDirectory()) {
 			return gallery.listFiles();
 		}
-		
+
 		return null;
 	}
-	
-	public String getVideoPath(){
+
+	public String getVideoPath() {
 		return getPath() + "/video.mp4";
 	}
-	
+
 	private List<KDFeaturePoint> featurePoints = null;
-	public List<KDFeaturePoint> getHotspotKDFeaturePoint(){
-		if(featurePoints != null){
+
+	public List<KDFeaturePoint> getKDFeaturePoint() {
+		if (featurePoints != null) {
 			return featurePoints;
 		}
-		
-		File siftFile = new File(getPath() + "/img.sift");
-		if(REMOTE_PROCESSOR_IP != null && !siftFile.exists()){
+
+		File siftFile = new File(getPath() + "/image.sift");
+		if (REMOTE_PROCESSOR_IP != null && !siftFile.exists()) {
 			try {
 				Socket socket = new Socket(REMOTE_PROCESSOR_IP, REMOTE_PROCESSOR_PORT);
 				OutputStream output = socket.getOutputStream();
-				
+
 				File imageFile = new File(getRawImage());
-				
+
 				byte[] length_array = new byte[4];
 				int length = (int) imageFile.length();
-				length_array[0] = (byte) (length >> 24);
-				length_array[1] = (byte) (length >> 16);
-				length_array[2] = (byte) (length >> 8);
-				length_array[3] = (byte) (length);
+				length_array[0] = (byte) ((length >>> 24) & 0xFF);
+				length_array[1] = (byte) ((length >>> 16) & 0xFF);
+				length_array[2] = (byte) ((length >>> 8) & 0xFF);
+				length_array[3] = (byte) ((length >>> 0) & 0xFF);
 				output.write(length_array);
-				
+
 				InputStream fileInput = new FileInputStream(imageFile);
 				byte[] buffer = new byte[1024];
-				
 				int offset = 0;
 				do {
-					int readLength = fileInput.read(buffer, offset, buffer.length);
+					int readLength = fileInput.read(buffer);
+					if (readLength == -1)
+						break;
 					output.write(buffer, 0, readLength);
 					offset += readLength;
 				} while (offset < length);
-				
+
 				fileInput.close();
-				
+
 				InputStream input = socket.getInputStream();
-				
-				OutputStream fileOutput = new FileOutputStream(imageFile);
+
+				OutputStream fileOutput = new FileOutputStream(siftFile);
 				offset = 0;
 				while (true) {
-					int readLength = input.read(buffer, offset, buffer.length);
+					int readLength = input.read(buffer, 0, buffer.length);
 					if (readLength == -1)
 						break;
 					fileOutput.write(buffer, 0, readLength);
@@ -123,7 +135,7 @@ public final class Capsule {
 				}
 				fileOutput.close();
 				socket.close();
-				
+
 			} catch (UnknownHostException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -132,27 +144,27 @@ public final class Capsule {
 				e.printStackTrace();
 			}
 		}
-		
-		if(!siftFile.exists()){
+
+		if (!siftFile.exists()) {
 			File imgFile = new File(getRawImage());
-			if(!imgFile.exists()){
+			if (!imgFile.exists()) {
 				return null;
 			}
 
 			Bitmap hotspotBitmap = BitmapFactory.decodeFile(imgFile.getPath());
 			featurePoints = calculateKDFeaturePoint(hotspotBitmap);
-			
+
 			try {
 				siftFile.createNewFile();
 			} catch (IOException e) {
 				e.printStackTrace();
 				return null;
 			}
-			
+
 			try {
 				FileOutputStream os = new FileOutputStream(siftFile);
 				Iterator<KDFeaturePoint> it = featurePoints.iterator();
-				while(it.hasNext()){
+				while (it.hasNext()) {
 					KDFeaturePoint fp = it.next();
 					JSONArray ap = new JSONArray();
 					ap.put(fp.dim);
@@ -160,13 +172,13 @@ public final class Capsule {
 					ap.put(Float.floatToIntBits(fp.y));
 					ap.put(Float.floatToIntBits(fp.scale));
 					ap.put(Float.floatToIntBits(fp.orientation));
-				
+
 					JSONArray desc = new JSONArray();
-					for(int i = 0; i < fp.descriptor.length; i++){
+					for (int i = 0; i < fp.descriptor.length; i++) {
 						desc.put(fp.descriptor[i]);
 					}
 					ap.put(desc);
-	
+
 					os.write(ap.toString().getBytes());
 					os.write('\n');
 				}
@@ -175,13 +187,13 @@ public final class Capsule {
 				return null;
 			}
 
-		}else{
+		} else {
 			featurePoints = new ArrayList<KDFeaturePoint>();
-			
+
 			try {
 				BufferedReader br = new BufferedReader(new FileReader(siftFile));
 				String line = null;
-				while((line = br.readLine()) != null){
+				while ((line = br.readLine()) != null) {
 					JSONArray arr = new JSONArray(line);
 
 					KDFeaturePoint fp = new KDFeaturePoint();
@@ -190,81 +202,81 @@ public final class Capsule {
 					fp.y = Float.intBitsToFloat(arr.getInt(2));
 					fp.scale = Float.intBitsToFloat(arr.getInt(3));
 					fp.orientation = Float.intBitsToFloat(arr.getInt(4));
-					
+
 					JSONArray desc = arr.getJSONArray(5);
 					fp.descriptor = new int[desc.length()];
-					for(int i = 0; i < desc.length(); i++){
+					for (int i = 0; i < desc.length(); i++) {
 						fp.descriptor[i] = desc.getInt(i);
 					}
-					
+
 					featurePoints.add(fp);
 				}
-				
+
 				br.close();
-				
+
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
 				return null;
-			} catch (IOException e){
+			} catch (IOException e) {
 				e.printStackTrace();
 				return null;
-			} catch (JSONException e){
+			} catch (JSONException e) {
 				e.printStackTrace();
 				return null;
 			}
 		}
-		
+
 		return featurePoints;
 	}
-	
-	public static String find(Bitmap bitmap){
+
+	public static String find(Bitmap bitmap) {
 		File storage = new File(Capsule.ROOT_PATH);
-		if(!storage.exists()){
+		if (!storage.exists()) {
 			storage.mkdirs();
 			return null;
 		}
 
 		List<KDFeaturePoint> snapshotFP = calculateKDFeaturePoint(bitmap);
 		KDTree snapshotKDTree = KDTree.createKDTree(snapshotFP);
-		if(snapshotKDTree == null){
+		if (snapshotKDTree == null) {
 			return null;
 		}
 
-	    int matchedKeyNum = MIN_MATCHED_KEY_NUM;
+		int matchedKeyNum = MIN_MATCHED_KEY_NUM;
 		String targetCapsule = null;
 		File[] capsuleDirs = storage.listFiles();
-		for(int i = 0; i < capsuleDirs.length; i++){
+		for (int i = 0; i < capsuleDirs.length; i++) {
 			File dir = capsuleDirs[i];
-			if(!dir.isDirectory()){
+			if (!dir.isDirectory()) {
 				continue;
 			}
-			
+
 			Capsule capsule = new Capsule(dir.getName());
-			List<KDFeaturePoint> spotFP = capsule.getHotspotKDFeaturePoint();
-			if(spotFP == null){
+			List<KDFeaturePoint> spotFP = capsule.getKDFeaturePoint();
+			if (spotFP == null) {
 				continue;
 			}
-			
-		    List<Match> ms = MatchKeys.findMatchesBBF(spotFP, snapshotKDTree);
-		    
-		    if(ms.size() > matchedKeyNum){
-		    	matchedKeyNum = ms.size();
-		    	targetCapsule = dir.getName();
-		    }
+
+			List<Match> ms = MatchKeys.findMatchesBBF(spotFP, snapshotKDTree);
+
+			if (ms.size() > matchedKeyNum) {
+				matchedKeyNum = ms.size();
+				targetCapsule = dir.getName();
+			}
 		}
 
 		return targetCapsule;
 	}
 
-	public static List<KDFeaturePoint> calculateKDFeaturePoint(Bitmap bitmap){
+	public static List<KDFeaturePoint> calculateKDFeaturePoint(Bitmap bitmap) {
 		RenderImage image = new RenderImage(bitmap);
-		image.scaleWithin(500);
+		image.scaleWithin(LOCAL_SIFT_IMAGE_PIXEL_LIMIT);
 		SIFT sift = new SIFT();
 		sift.detectFeatures(image.toPixelFloatArray(null));
 		return sift.getGlobalKDFeaturePoints();
 	}
 
-	protected static String generateId(){
+	protected static String generateId() {
 		while (true) {
 			StringBuffer buffer = new StringBuffer();
 			for (int i = 0; i < 16; i++) {
@@ -277,7 +289,7 @@ public final class Capsule {
 					buffer.append((char) ('0' + ascii - 52));
 				}
 			}
-			
+
 			String result = buffer.toString();
 			File file = new File(ROOT_PATH + "/" + result);
 			if (!file.exists())
